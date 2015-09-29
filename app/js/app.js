0aceb9e6285675d043cae9a04d4caac5bbf07137
Cameron Coats <c@meroncoats.co.uk>
2015-09-29 14:32:21 +0100

    extra {

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,12 @@
 // define the app
 // =============================================================================
-var app = angular.module('gwa-rm', ['ui.router', 'ui.bootstrap', 'ui.mask', 'ngSanitize','angular-toasty'])
+var app = angular.module('gwa-rm', ['ui.router', 'ui.bootstrap', 'ui.mask', 'ngSanitize', 'angular-toasty'])
 
 // routes
 // =============================================================================
-.config(function($stateProvider, $urlRouterProvider) {
-    //top level states
-    $stateProvider
+.config(function ($stateProvider, $urlRouterProvider) {
+    // top level states
+  $stateProvider
       .state('home', {
         url: '/home',
         templateUrl: 'partials/home.html',
@@ -18,57 +18,59 @@ var app = angular.module('gwa-rm', ['ui.router', 'ui.bootstrap', 'ui.mask', 'ngS
         controller: 'mainController'
       })
       // catchall state
-    $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home');
   })
-// Main controller
-// =============================================================================
-  .controller('mainController', function($scope, $http, $q, $window, $location, $interval) {
+  // Main controller
+  // =============================================================================
+  .controller('mainController', function ($scope, $http, $q, $window, $location, $interval) {
     /////////////////// Buttons ///////////////////////
     //
     //
-    $scope.buttons = [];
-    $scope.isHome = function(){
-      $scope.button='home';
+      $scope.buttons = [];
+    $scope.isHome = function () {
+      $scope.button = 'home';
       toasty.success({
-    title: "Welcome Back",
-    msg: "We'll stop flashing lights n shit now",
-    showClose: false,
-    clickToClose: true,
-    timeout: 1000,
-    sound: true,
-    html: false,
-    shake: false,
-    theme: "material"
-});};
+        title: 'Welcome Back',
+        msg: "We'll stop flashing lights n shit now",
+        showClose: false,
+        clickToClose: true,
+        timeout: 1000,
+        sound: true,
+        html: false,
+        shake: false,
+        theme: "material"
+      });
+    };
 
-$scope.isAway = function(){
-  $scope.button='away';
-  toasty.success({
-title: "You're Out",
-msg: "We'll keep your home safe",
-showClose: false,
-clickToClose: true,
-timeout: 1000,
-sound: true,
-html: false,
-shake: false,
-theme: "material"
-});};
-    }
+    $scope.isAway = function () {
+      $scope.button = 'away';
+      toasty.success({
+        title: "You're Out",
+        msg: "We'll keep your home safe",
+        showClose: false,
+        clickToClose: true,
+        timeout: 1000,
+        sound: true,
+        html: false,
+        shake: false,
+        theme: "material"
+      });
+    };
+
     /////////////////// Alerts ///////////////////////
     //
     // object to hold alerts
     $scope.alerts = [];
     // function to handle closing alerts
-    $scope.closeAlert = function(index) {
+    $scope.closeAlert = function (index) {
       $scope.alerts.splice(index, 1);
     };
 
-  }).directive('capitalize', function() {
+  }).directive('capitalize', function () {
     return {
       require: 'ngModel',
-      link: function(scope, element, attrs, modelCtrl) {
-        var capitalize = function(inputValue) {
+      link: function (scope, element, attrs, modelCtrl) {
+        var capitalize = function (inputValue) {
           if (inputValue == undefined) inputValue = '';
           var capitalized = inputValue.toUpperCase();
           if (capitalized !== inputValue) {
