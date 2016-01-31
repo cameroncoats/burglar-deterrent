64469fa19eac05df21803af314262e35110bcc48
Cameron Coats <c@meroncoats.co.uk>
2016-01-31 17:15:22 +0000

    branding & beautify

--- app/apiv1/handlers/HtmlHandler.php
+++ app/apiv1/handlers/HtmlHandler.php
@@ -64,7 +64,7 @@ class HtmlHandler extends BaseHandler
 <!DOCTYPE html>
 <html>
 <head>
-    <title>API v1</title>
+    <title>Eversafe API v1</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css" rel="stylesheet" integrity="sha256-hMIwZV8FylgKjXnmRI2YY0HLnozYr7Cuo1JvRtzmPWs= sha512-k+wW4K+gHODPy/0gaAMUNmCItIunOZ+PeLW7iZwkDZH/wMaTrSJTt7zK6TGy6p+rnDBghAxdvu1LX2Ohg0ypDw==" crossorigin="anonymous">
 </head>
@@ -74,7 +74,7 @@ class HtmlHandler extends BaseHandler
 <div class="row">
 <div class="col-xs-10 col-xs-offset-1">
 
-<h1>GWA-RM API v1<br><small>
+<h1>Eversafe API v1<br><small>
 EOT
 .$_SERVER['PATH_INFO'].
 <<<EOT
--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,9 @@ var app = angular.module('gwa-rm', ['ui.router', 'ui.bootstrap', 'ui.mask', 'ngS
 
 // routes
 // =============================================================================
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
     // top level states
-  $stateProvider
+    $stateProvider
       .state('home', {
         url: '/home',
         templateUrl: 'partials/home.html',
@@ -18,74 +18,74 @@ var app = angular.module('gwa-rm', ['ui.router', 'ui.bootstrap', 'ui.mask', 'ngS
         controller: 'mainController'
       })
       // catchall state
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
   })
   .config(['toastyConfigProvider', function(toastyConfigProvider) {
     toastyConfigProvider.setConfig({
-        limit:1
+      limit: 1
     });
-}])
+  }])
   // Main controller
   // =============================================================================
-  .controller('mainController', function ($scope, $http, $q, $window, $location, $interval, toasty) {
+  .controller('mainController', function($scope, $http, $q, $window, $location, $interval, toasty) {
     /////////////////// Buttons ///////////////////////
     //
     //
-      $scope.button=[];
-      $http.get("apiv1/status/1").then(function(response) {
-          $scope.button = response.data;
-        })
-    $scope.isHome = function () {
-      if($scope.button!='home'){
+    $scope.button = [];
+    $http.get("apiv1/status/1").then(function(response) {
+      $scope.button = response.data;
+    })
+    $scope.isHome = function() {
+      if ($scope.button != 'home') {
         $http.put("apiv1/status/1/home");
-      $scope.button = 'home';
-      toasty.success({
-        title: 'Welcome Back',
-        showClose: false,
-        clickToClose: true,
-        timeout: 2500,
-        sound: true,
-        html: false,
-        shake: false,
-        limit: 1,
-        theme: "material"
-      });
-    }
-   };
+        $scope.button = 'home';
+        toasty.success({
+          title: 'Welcome Back',
+          showClose: false,
+          clickToClose: true,
+          timeout: 2500,
+          sound: true,
+          html: false,
+          shake: false,
+          limit: 1,
+          theme: "material"
+        });
+      }
+    };
 
-    $scope.isAway = function () {
-      if($scope.button!='away'){
-      $http.put("apiv1/status/1/away");
-      $scope.button = 'away';
-      toasty.success({
-        title: "You're Out",
-        msg: "We'll keep your home safe",
-        showClose: false,
-        clickToClose: true,
-        timeout: 2500,
-        sound: true,
-        html: false,
-        shake: false,
-        limit: 1,
-        theme: "material"
-      });
-    }
-  };
+    $scope.isAway = function() {
+      if ($scope.button != 'away') {
+        $http.put("apiv1/status/1/away");
+        $scope.button = 'away';
+        toasty.success({
+          title: "You're Out",
+          msg: "We'll keep your home safe",
+          showClose: false,
+          clickToClose: true,
+          timeout: 2500,
+          sound: true,
+          html: false,
+          shake: false,
+          limit: 1,
+          theme: "material"
+        });
+      }
+    };
 
     /////////////////// Alerts ///////////////////////
     //
     // object to hold alerts
     $scope.alerts = [];
     // function to handle closing alerts
-    $scope.closeAlert = function (index) {
+    $scope.closeAlert = function(index) {
       $scope.alerts.splice(index, 1);
     };
 
-  }).directive('capitalize', function () {
+  }).directive('capitalize', function() {
     return {
       require: 'ngModel',
-      link: function (scope, element, attrs, modelCtrl) {
-        var capitalize = function (inputValue) {
+      link: function(scope, element, attrs, modelCtrl) {
+        var capitalize = function(inputValue) {
           if (inputValue == undefined) inputValue = '';
           var capitalized = inputValue.toUpperCase();
           if (capitalized !== inputValue) {
