f19b7abb743f0105ef23985d9faf0f693401e0e2
Cameron Coats <c@meroncoats.co.uk>
2016-02-02 10:00:27 +0000

    series labels

--- web/app/js/z-app.js
+++ web/app/js/z-app.js
@@ -52,7 +52,7 @@ $('#energyNow').each(function(e){
       $scope.button = response.data;
     })
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.series = ['Energy Use', 'Energy Cost'];
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
