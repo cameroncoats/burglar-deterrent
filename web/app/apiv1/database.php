a43db2e31057fb356500d6c0e33cfb8c0794055f
Cameron Coats <c@meroncoats.co.uk>
2016-02-02 10:25:53 +0000

    use mysql

--- web/app/apiv1/database.php
+++ web/app/apiv1/database.php
@@ -1,4 +1,12 @@
 <?php
+<?php
 //Database connection Settings
 $database=array();
-$database['connstring']="sqlite:database.sqlite";
+$database['name']='eversafe'; // Database Name
+$database['username']='eversafe'; // Username - leave blank if MSSQL & windows auth
+$database['password']='safeever'; // Password - leave blank if MSSQL & windows auth
+$database['host']='localhost'; // database Host
+$database['driver']='mysql'; // database Driver - mysql, sqlsrv etc
+// PDO connection string - this is correct for mysql but might need tweaked for
+// MSSQL etc
+$database['connstring']=$database['driver'].":host=".$database['host'].";dbname=".$database['name'];
