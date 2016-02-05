a4baa374392eaf500ebb146e7b6a6a67a0f2f7ae
Cameron Coats <c@meroncoats.co.uk>
2016-02-05 11:13:23 +0000

    fix parameter types

--- web/app/apiv1/models/TSDModel.php
+++ web/app/apiv1/models/TSDModel.php
@@ -4,8 +4,8 @@ class TSDModel extends BaseModel
 public function addData($chipid,$value){
  $sql = "INSERT INTO `tblTSDB` (`tsTime`,`tsData`,`tsChipID`) VALUES (CURRENT_TIMESTAMP,:data,:chip)";
  $sth = $this->_db->prepare($sql);
- $sth -> bindParam(":data",$value,PDO::PARAM_FLOAT);
- $sth -> bindParam(":chip",$chipid,PDO::PARAM_FLOAT);
+ $sth -> bindParam(":data",$value,PDO::PARAM_STR);
+ $sth -> bindParam(":chip",$chipid,PDO::PARAM_INT);
  $sth -> execute();
 }
 
