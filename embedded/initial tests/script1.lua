d8a9df46916454106e24b832aab7b3c20cf9b1b5
Cameron Coats <c@meroncoats.co.uk>
2016-02-08 22:42:18 +0000

    send ADC value in place of power

--- embedded/initial tests/script1.lua
+++ embedded/initial tests/script1.lua
@@ -2,13 +2,13 @@ status = '/"away'
 pin=4
 id = node.chipid()
 gpio.mode(pin,gpio.OUTPUT)
-wifi.sta.config("Cameron's iPhone","isles6464")
+wifi.sta.config("ASK4 Wireless","")
 tmr.alarm(1,2000,1,function()
-
+pwr = adc.read(0)
 sk=net.createConnection(net.TCP, 0)
 sk:on("receive", function(sck, c) status = string.match(c, '\"....') end )
 sk:connect(80,"77.95.37.3")
-sk:send("GET /apiv1/status/"..id.."/59.6 HTTP/1.1\r\nHost: eversafe.decision.hosting\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n")
+sk:send("GET /apiv1/status/"..id.."/"..pwr.." HTTP/1.1\r\nHost: eversafe.decision.hosting\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n")
 if status~=nil then 
 status = string.sub(status,2)
 end
