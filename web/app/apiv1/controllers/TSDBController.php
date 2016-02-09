c23ad7232df34bd5a3250c9cc46304e34f55b3c5
Cameron Coats <c@meroncoats.co.uk>
2016-02-09 15:48:50 +0000

    indents & fix variable

--- web/app/apiv1/controllers/TSDBController.php
+++ web/app/apiv1/controllers/TSDBController.php
@@ -4,25 +4,26 @@ class TSDBController extends BaseController
 {
     public function getAction($request)
     {
-        $tsdb = new TSDModel();
-        switch ($url_elements[2]) {
-      case 'week':
-      return $tsdb->getLastWeek($request->url_elements[3]);
-      break;
-      case 'day':
-      return $tsdb->getLastDay($request->url_elements[3]);
-      break;
-      case 'listPlugs':
-      return $tsdb->getUsersPlugs($request->url_elements[3]);
-      break;
-      case 'current':
-      return $tsdb->getMostRecentPower($request->url_elements[3]);
-      break;
-      case 'currentUser':
-      return $tsdb->getMostRecentPowerUser($request->url_elements[3]);
-      break;
-      default:
-      $this->badRequest('Need to specify time period and user id!');
-    }
+      $tsdb = new TSDModel();
+      switch ($request->url_elements[2]) {
+          case 'week':
+            return $tsdb->getLastWeek($request->url_elements[3]);
+          break;
+          case 'day':
+            return $tsdb->getLastDay($request->url_elements[3]);
+          break;
+          case 'listPlugs':
+            return $tsdb->getUsersPlugs($request->url_elements[3]);
+          break;
+          case 'current':
+            return $tsdb->getMostRecentPower($request->url_elements[3]);
+          break;
+          case 'currentUser':
+            return $tsdb->getMostRecentPowerUser($request->url_elements[3]);
+          break;
+          default:
+            $this->badRequest('Need to specify time period and user id!');
+          break;
+      }
     }
 }
