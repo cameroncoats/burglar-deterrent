5d3d89e440866cf3ee2ce8eed4026e14ae2a138f
Cameron Coats <c@meroncoats.co.uk>
2017-02-11 10:16:27 +0000

    Add comment

--- web/app/apiv1/database.php
+++ web/app/apiv1/database.php
@@ -1,5 +1,10 @@
 <?php
 //Database connection Settings
+
+// Yes, there are passwords in the commit history
+// No, they won't work anymore :)
+// Note to self: sanitise before committing 
+
 $database=array();
 $database['name']='eversafe'; // Database Name
 $database['username']='eversafe'; // Username - leave blank if MSSQL & windows auth
